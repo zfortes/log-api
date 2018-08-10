35ad87286d5acefdab0d3c4e7eeb3930c0cd4c6a
Jose Fortes <josefortes@iMac-de-Daniel.local>
2018-08-10 16:24:22 -0300

    Arquivos adicionados

---
+++ src/main/java/com/fortes/log/api/LogApiApplication.java
@@ -0,0 +1,12 @@
+package com.fortes.log.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fortes/log/api/LogApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fortes.log.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LogApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
