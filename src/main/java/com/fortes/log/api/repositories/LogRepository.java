170504fa7365948c57d1769b17bfac02740ab443
Jose Fortes <josefortes@iMac-de-Daniel.local>
2018-08-20 11:45:16 -0300

    Mudancas no log direto no tomcat. Classe LogFactory implementada

--- src/main/java/com/fortes/log/api/repositories/LogRepository.java
+++ src/main/java/com/fortes/log/api/repositories/LogRepository.java
@@ -1,10 +1,15 @@
 package com.fortes.log.api.repositories;
 import com.fortes.log.api.entidies.Log;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface LogRepository extends JpaRepository<Log, Long>{
 //	public List<Log> findAll(Long id, Pageable pageable);
+	
+//	List<Log> findById(Long id);
 }
--- src/main/java/com/fortes/log/api/services/impl/LogServiceImpl.java
+++ src/main/java/com/fortes/log/api/services/impl/LogServiceImpl.java
@@ -4,10 +4,12 @@ import com.fortes.log.api.entidies.Log;
 import com.fortes.log.api.repositories.LogRepository;
 import com.fortes.log.api.services.LogService;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.transaction.Transactional;
@@ -15,6 +17,10 @@ import javax.transaction.Transactional;
 
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class LogServiceImpl implements LogService{
@@ -22,16 +28,32 @@ public class LogServiceImpl implements LogService{
 	@Autowired
 	private LogRepository rp;
 	
+	private static final Logger logTela = LoggerFactory.getLogger(LogServiceImpl.class);
 //	@Autowired
 //	private LogPageAbleRepository rp2;
 
 	
 	@Transactional
-	public Log salvar(Log log){	
+	public Log salvar(Log log){
+//		BindingResult result;
 		Timestamp dataDeHoje = new Timestamp(System.currentTimeMillis());
 		dataDeHoje.toLocalDateTime();
-		log.setDataHora(dataDeHoje);;
-		return rp.save(log);	
+		log.setDataHora(dataDeHoje);
+//		
+		
+		
+		
+		Optional<Log> resultado = rp.findById(rp.save(log).getId());
+		
+//		
+		
+		
+		if (resultado.isPresent()){
+			logTela.info("_________________Erro ao tentar salvar no banco______________________");
+		}
+
+		
+		return 	log;
 	}
 	
 	@Transactional
@@ -44,5 +66,5 @@ public class LogServiceImpl implements LogService{
 		@SuppressWarnings("deprecation")
 		PageRequest pageRequest = new PageRequest(p, tp);
 		return rp.findAll(pageRequest);
-	}	
+	}
 }
